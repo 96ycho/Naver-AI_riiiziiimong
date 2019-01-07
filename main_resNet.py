bf07f6a94a9f5fe9c0d943a746f7b6dc6ba213a1
조은지 <whdmswl96@gmail.com>
2019-01-07 22:28:14 +0900

    resnet

---
+++ main_resNet.py
@@ -0,0 +1,543 @@
+# -*- coding: utf_8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import cv2
+import argparse
+import pickle
+
+import nsml
+import numpy as np
+
+from nsml import DATASET_PATH
+import keras
+from keras.models import Sequential
+from keras.models import Model
+
+from keras.layers import Dense, Dropout, BatchNormalization, Flatten, Activation
+from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Input
+from keras.callbacks import ReduceLROnPlateau
+from keras import backend as K
+from data_loader import train_data_loader, test_data_loader
+from keras.regularizers import l2
+
+import keras.backend as K
+
+def get_categorical_accuracy_keras(y_true, y_pred):
+    return K.mean(K.equal(K.argmax(y_true, axis=1), K.argmax(y_pred, axis=1)))
+
+import numpy as np
+
+def get_categorical_accuracy(y_true, y_pred):
+    return (np.argmax(y_true, axis=1) == np.argmax(y_pred, axis=1)).mean()
+
+def bind_model(model):
+    def save(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+        model.save_weights(os.path.join(dir_name, 'model'))
+        print('model saved!')
+
+    def load(file_path):
+        model.load_weights(file_path)
+        print('model loaded!')
+
+    def infer(queries, db):
+
+        # Query 개수: 195
+        # Reference(DB) 개수: 1,127
+        # Total (query + reference): 1,322
+
+        queries, query_img, references, reference_img = preprocess(queries, db)
+
+        print('test data load queries {} query_img {} references {} reference_img {}'.
+              format(len(queries), len(query_img), len(references), len(reference_img)))
+
+        queries = np.asarray(queries)
+        query_img = np.asarray(query_img)
+        references = np.asarray(references)
+        reference_img = np.asarray(reference_img)
+
+        query_img = query_img.astype('float32')
+        query_img /= 255
+        reference_img = reference_img.astype('float32')
+        reference_img /= 255
+
+        get_feature_layer = K.function([model.layers[0].input] + [K.learning_phase()], [model.layers[-2].output])
+
+        print('inference start')
+
+        # inference
+        query_vecs = get_feature_layer([query_img, 0])[0]
+
+        # caching db output, db inference
+        db_output = './db_infer.pkl'
+        if os.path.exists(db_output):
+            with open(db_output, 'rb') as f:
+                reference_vecs = pickle.load(f)
+        else:
+            reference_vecs = get_feature_layer([reference_img, 0])[0]
+            with open(db_output, 'wb') as f:
+                pickle.dump(reference_vecs, f)
+
+        # l2 normalization
+        query_vecs = l2_normalize(query_vecs)
+        reference_vecs = l2_normalize(reference_vecs)
+
+        # Calculate cosine similarity
+        sim_matrix = np.dot(query_vecs, reference_vecs.T)
+
+        retrieval_results = {}
+
+        for (i, query) in enumerate(queries):
+            query = query.split('/')[-1].split('.')[0]
+            sim_list = zip(references, sim_matrix[i].tolist())
+            sorted_sim_list = sorted(sim_list, key=lambda x: x[1], reverse=True)
+
+            ranked_list = [k.split('/')[-1].split('.')[0] for (k, v) in sorted_sim_list]  # ranked list
+
+            retrieval_results[query] = ranked_list
+        print('done')
+
+        return list(zip(range(len(retrieval_results)), retrieval_results.items()))
+
+    # DONOTCHANGE: They are reserved for nsml
+    nsml.bind(save=save, load=load, infer=infer)
+
+
+def l2_normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+# data preprocess
+def preprocess(queries, db):
+    query_img = []
+    reference_img = []
+    img_size = (224, 224)
+
+    for img_path in queries:
+        img = cv2.imread(img_path, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, img_size)
+        query_img.append(img)
+
+    for img_path in db:
+        img = cv2.imread(img_path, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, img_size)
+        reference_img.append(img)
+
+    return queries, query_img, db, reference_img
+
+
+def resnet_layer(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
+    """2D Convolution-Batch Normalization-Activation stack builder
+
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+
+    # Returns
+        x (tensor): tensor as input to the next layer
+    """
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l2(1e-4))
+
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
+
+
+def resnet_v1(input_shape, depth, num_classes=10):
+    """ResNet Version 1 Model builder [a]
+
+    Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
+    Last ReLU is after the shortcut connection.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filters is
+    doubled. Within each stage, the layers have the same number filters and the
+    same number of filters.
+    Features maps sizes:
+    stage 0: 32x32, 16
+    stage 1: 16x16, 32
+    stage 2:  8x8,  64
+    The Number of parameters is approx the same as Table 6 of [a]:
+    ResNet20 0.27M
+    ResNet32 0.46M
+    ResNet44 0.66M
+    ResNet56 0.85M
+    ResNet110 1.7M
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 6 != 0:
+        raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
+    # Start model definition.
+    num_filters = 16
+    num_res_blocks = int((depth - 2) / 6)
+
+    inputs = Input(shape=input_shape)
+    x = resnet_layer(inputs=inputs)
+    # Instantiate the stack of residual units
+    for stack in range(3):
+        for res_block in range(num_res_blocks):
+            strides = 1
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                strides = 2  # downsample
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters,
+                             strides=strides)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters,
+                             activation=None)
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = keras.layers.add([x, y])
+            x = Activation('relu')(x)
+        num_filters *= 2
+
+    # Add classifier on top.
+    # v1 does not use BN after last shortcut connection-ReLU
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+
+    # eunji
+    y = Dense(512)(y)
+    y = Activation('relu')(y)
+    y = Dropout(0.5)(y)
+    y = Dense(num_classes)(y)
+    outputs = Activation('softmax')(y)
+
+    """
+    y = Activation('softmax')(y)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(y)
+    """
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def resnet_v2(input_shape, depth, num_classes=10):
+    """ResNet Version 2 Model builder [b]
+
+    Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
+    bottleneck layer
+    First shortcut connection per layer is 1 x 1 Conv2D.
+    Second and onwards shortcut connection is identity.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filter maps is
+    doubled. Within each stage, the layers have the same number filters and the
+    same filter map sizes.
+    Features maps sizes:
+    conv1  : 32x32,  16
+    stage 0: 32x32,  64
+    stage 1: 16x16, 128
+    stage 2:  8x8,  256
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 9 != 0:
+        raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
+    # Start model definition.
+    num_filters_in = 16
+    num_res_blocks = int((depth - 2) / 9)
+
+    inputs = Input(shape=input_shape)
+    # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
+    x = resnet_layer(inputs=inputs,
+                     num_filters=num_filters_in,
+                     conv_first=True)
+
+    # Instantiate the stack of residual units
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:  # first layer and first stage
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:  # first layer but not first stage
+                    strides = 2    # downsample
+
+            # bottleneck residual unit
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters_in,
+                             kernel_size=1,
+                             strides=strides,
+                             activation=activation,
+                             batch_normalization=batch_normalization,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_in,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_out,
+                             kernel_size=1,
+                             conv_first=False)
+            if res_block == 0:
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters_out,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = keras.layers.add([x, y])
+
+        num_filters_in = num_filters_out
+
+    # Add classifier on top.
+    # v2 has BN-ReLU before Pooling
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+
+    # eunji
+    y = Dense(512)(y)
+    y = Activation('relu')(y)
+    y = Dropout(0.5)(y)
+    y = Dense(num_classes)(y)
+    outputs = Activation('softmax')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+
+    # hyperparameters
+    args.add_argument('--epochs', type=int, default=5)
+    args.add_argument('--batch_size', type=int, default=128)
+
+    # DONOTCHANGE: They are reserved for nsml
+    args.add_argument('--mode', type=str, default='train', help='submit일때 해당값이 test로 설정됩니다.')
+    args.add_argument('--iteration', type=str, default='0', help='fork 명령어를 입력할때의 체크포인트로 설정됩니다. 체크포인트 옵션을 안주면 마지막 wall time 의 model 을 가져옵니다.')
+    args.add_argument('--pause', type=int, default=0, help='model 을 load 할때 1로 설정됩니다.')
+    config = args.parse_args()
+
+    # training parameters
+    nb_epoch = config.epochs
+    nb_epoch = 500
+    batch_size = config.batch_size
+    num_classes = 1000
+    input_shape = (224, 224, 3)  # input image shape
+
+    # eunji
+    version = 2
+    n=3
+
+    if version == 1:
+        depth = n * 6 + 2
+    elif version == 2:
+        depth = n * 9 + 2
+
+    """ Model """
+    """
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same', input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+    model.summary()
+    """
+
+    model = resnet_v2(input_shape=input_shape, depth=depth, num_classes=num_classes)
+    model.summary()
+
+    bind_model(model)
+
+    if config.pause:
+        nsml.paused(scope=locals())
+
+    bTrainmode = False
+    if config.mode == 'train':
+        bTrainmode = True
+
+        """ Initiate RMSprop optimizer """
+        opt = keras.optimizers.rmsprop(lr=0.00045, decay=1e-6)
+        model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=[get_categorical_accuracy_keras])
+        # model.compile(loss='categorical_crossentropy',
+        #               optimizer=opt,
+        #               metrics=['accuracy'])
+
+        """ Load data """
+        print('dataset path', DATASET_PATH)
+        output_path = ['./img_list.pkl', './label_list.pkl']
+        train_dataset_path = DATASET_PATH + '/train/train_data'
+
+        if nsml.IS_ON_NSML:
+            # Caching file
+            nsml.cache(train_data_loader, data_path=train_dataset_path, img_size=input_shape[:2],
+                       output_path=output_path)
+        else:
+            # local에서 실험할경우 dataset의 local-path 를 입력해주세요.
+            train_data_loader(train_dataset_path, input_shape[:2], output_path=output_path)
+
+        with open(output_path[0], 'rb') as img_f:
+            img_list = pickle.load(img_f)
+        with open(output_path[1], 'rb') as label_f:
+            label_list = pickle.load(label_f)
+
+        x_train = np.asarray(img_list)
+        labels = np.asarray(label_list)
+        y_train = keras.utils.to_categorical(labels, num_classes=num_classes)
+        x_train = x_train.astype('float32')
+        x_train /= 255
+        print(len(labels), 'train samples')
+
+        """ Callback """
+        monitor = 'get_categorical_accuracy_keras'
+        reduce_lr = ReduceLROnPlateau(monitor=monitor, patience=3)
+
+        """ Training loop """
+        for epoch in range(nb_epoch):
+            res = model.fit(x_train, y_train,
+                            batch_size=batch_size,
+                            initial_epoch=epoch,
+                            epochs=epoch + 1,
+                            callbacks=[reduce_lr],
+                            verbose=1,
+                            shuffle=True)
+            print(res.history)
+            train_loss, train_acc = res.history['loss'][0], res.history['get_categorical_accuracy_keras'][0]
+            nsml.report(summary=True, epoch=epoch, epoch_total=nb_epoch, loss=train_loss, acc=train_acc)
+            nsml.save(epoch)
+
+    # eunji
+    """
+    config.mode = 'test'
+    if config.mode  == 'test':
+        queries, db = test_data_loader('../../Data_example_test')
+
+        # Query 개수: 195
+        # Reference(DB) 개수: 1,127
+        # Total (query + reference): 1,322
+
+        queries, query_img, references, reference_img = preprocess(queries, db)
+
+        print('test data load queries {} query_img {} references {} reference_img {}'.
+              format(len(queries), len(query_img), len(references), len(reference_img)))
+
+        queries = np.asarray(queries)
+        query_img = np.asarray(query_img)
+        references = np.asarray(references)
+        reference_img = np.asarray(reference_img)
+
+        query_img = query_img.astype('float32')
+        query_img /= 255
+        reference_img = reference_img.astype('float32')
+        reference_img /= 255
+
+        get_feature_layer = K.function([model.layers[0].input] + [K.learning_phase()], [model.layers[-2].output])
+
+        print('inference start')
+
+        # inference
+        query_vecs = get_feature_layer([query_img, 0])[0]
+
+        # caching db output, db inference
+        db_output = './db_infer.pkl'
+        if os.path.exists(db_output):
+            with open(db_output, 'rb') as f:
+                reference_vecs = pickle.load(f)
+        else:
+            reference_vecs = get_feature_layer([reference_img, 0])[0]
+            with open(db_output, 'wb') as f:
+                pickle.dump(reference_vecs, f)
+
+        # l2 normalization
+        query_vecs = l2_normalize(query_vecs)
+        reference_vecs = l2_normalize(reference_vecs)
+
+        # Calculate cosine similarity
+        sim_matrix = np.dot(query_vecs, reference_vecs.T)
+
+        retrieval_results = {}
+
+        for (i, query) in enumerate(queries):
+            query = query.split('/')[-1].split('.')[0]
+            sim_list = zip(references, sim_matrix[i].tolist())
+            sorted_sim_list = sorted(sim_list, key=lambda x: x[1], reverse=True)
+
+            ranked_list = [k.split('/')[-1].split('.')[0] for (k, v) in sorted_sim_list]  # ranked list
+
+            retrieval_results[query] = ranked_list
+        print('done')
+    """
