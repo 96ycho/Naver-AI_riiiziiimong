fcdb943a5d2e5e51294bf379089dc1ff0d7f3dbc
gyuri <misspyun1@gmail.com>
2019-01-07 20:12:08 +0900

    main_denseNet

---
+++ main_denseNet.py
@@ -0,0 +1,389 @@
+# -*- coding: utf_8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import cv2
+import argparse
+import pickle
+
+import nsml
+import numpy as np
+
+from nsml import DATASET_PATH
+import keras
+from keras.models import Sequential, Model
+from keras.layers import Input, merge,concatenate, ZeroPadding2D, Dense, Dropout, Flatten, Activation
+
+from keras.layers.pooling import AveragePooling2D, GlobalAveragePooling2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+
+from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import ReduceLROnPlateau
+from keras import backend as K
+from data_loader import train_data_loader
+#import keras_densenet.models
+
+
+import keras.backend as K
+
+from custom_layers import Scale
+
+def DenseNet(nb_dense_block=4, growth_rate=32, nb_filter=64, reduction=0.0, dropout_rate=0.0, weight_decay=1e-4, classes=1000, weights_path=None):
+    '''Instantiate the DenseNet 121 architecture,
+        # Arguments
+            nb_dense_block: number of dense blocks to add to end
+            growth_rate: number of filters to add per dense block
+            nb_filter: initial number of filters
+            reduction: reduction factor of transition blocks.
+            dropout_rate: dropout rate
+            weight_decay: weight decay factor
+            classes: optional number of classes to classify images
+            weights_path: path to pre-trained weights
+        # Returns
+            A Keras model instance.
+    '''
+    eps = 1.1e-5
+
+    # compute compression factor
+    compression = 1.0 - reduction
+
+    # Handle Dimension Ordering for different backends
+    global concat_axis
+    if K.image_dim_ordering() == 'tf':
+      concat_axis = 3
+      img_input = Input(shape=(224, 224, 3), name='data')
+    else:
+      concat_axis = 1
+      img_input = Input(shape=(3, 224, 224), name='data')
+
+    # From architecture for ImageNet (Table 1 in the paper)
+    nb_filter = 64
+    nb_layers = [6,12,24,16] # For DenseNet-121
+
+    # Initial convolution
+    x = ZeroPadding2D((3, 3), name='conv1_zeropadding')(img_input)
+    x = Conv2D(nb_filter,( 7, 7), strides = (2,2), name='conv1', use_bias=False)(x)
+    x = BatchNormalization(epsilon=eps, axis=concat_axis, name='conv1_bn')(x)
+    x = Scale(axis=concat_axis, name='conv1_scale')(x)
+    x = Activation('relu', name='relu1')(x)
+    x = ZeroPadding2D((1, 1), name='pool1_zeropadding')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2), name='pool1')(x)
+
+    # Add dense blocks
+    for block_idx in range(nb_dense_block - 1):
+        stage = block_idx+2
+        x, nb_filter = dense_block(x, stage, nb_layers[block_idx], nb_filter, growth_rate, dropout_rate=dropout_rate, weight_decay=weight_decay)
+
+        # Add transition_block
+        x = transition_block(x, stage, nb_filter, compression=compression, dropout_rate=dropout_rate, weight_decay=weight_decay)
+        nb_filter = int(nb_filter * compression)
+
+    final_stage = stage + 1
+    x, nb_filter = dense_block(x, final_stage, nb_layers[-1], nb_filter, growth_rate, dropout_rate=dropout_rate, weight_decay=weight_decay)
+
+    x = BatchNormalization(epsilon=eps, axis=concat_axis, name='conv'+str(final_stage)+'_blk_bn')(x)
+    x = Scale(axis=concat_axis, name='conv'+str(final_stage)+'_blk_scale')(x)
+    x = Activation('relu', name='relu'+str(final_stage)+'_blk')(x)
+    x = GlobalAveragePooling2D(name='pool'+str(final_stage))(x)
+
+    x = Dense(classes, name='fc6')(x)
+    x = Activation('softmax', name='prob')(x)
+
+    model = Model(img_input, x, name='densenet')
+
+    if weights_path is not None:
+      model.load_weights(weights_path)
+
+    return model
+
+
+def conv_block(x, stage, branch, nb_filter, dropout_rate=None, weight_decay=1e-4):
+    '''Apply BatchNorm, Relu, bottleneck 1x1 Conv2D, 3x3 Conv2D, and option dropout
+        # Arguments
+            x: input tensor
+            stage: index for dense block
+            branch: layer index within each dense block
+            nb_filter: number of filters
+            dropout_rate: dropout rate
+            weight_decay: weight decay factor
+    '''
+    eps = 1.1e-5
+    conv_name_base = 'conv' + str(stage) + '_' + str(branch)
+    relu_name_base = 'relu' + str(stage) + '_' + str(branch)
+
+    # 1x1 Convolution (Bottleneck layer)
+    inter_channel = nb_filter * 4
+    x = BatchNormalization(epsilon=eps, axis=concat_axis, name=conv_name_base+'_x1_bn')(x)
+    x = Scale(axis=concat_axis, name=conv_name_base+'_x1_scale')(x)
+    x = Activation('relu', name=relu_name_base+'_x1')(x)
+    x = Conv2D(inter_channel,( 1, 1), name=conv_name_base+'_x1', use_bias=False)(x)
+
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+
+    # 3x3 Convolution
+    x = BatchNormalization(epsilon=eps, axis=concat_axis, name=conv_name_base+'_x2_bn')(x)
+    x = Scale(axis=concat_axis, name=conv_name_base+'_x2_scale')(x)
+    x = Activation('relu', name=relu_name_base+'_x2')(x)
+    x = ZeroPadding2D((1, 1), name=conv_name_base+'_x2_zeropadding')(x)
+    x = Conv2D(nb_filter,( 3, 3), name=conv_name_base+'_x2', use_bias=False)(x)
+
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+
+    return x
+
+
+def transition_block(x, stage, nb_filter, compression=1.0, dropout_rate=None, weight_decay=1E-4):
+    ''' Apply BatchNorm, 1x1 Convolution, averagePooling, optional compression, dropout
+        # Arguments
+            x: input tensor
+            stage: index for dense block
+            nb_filter: number of filters
+            compression: calculated as 1 - reduction. Reduces the number of feature maps in the transition block.
+            dropout_rate: dropout rate
+            weight_decay: weight decay factor
+    '''
+
+    eps = 1.1e-5
+    conv_name_base = 'conv' + str(stage) + '_blk'
+    relu_name_base = 'relu' + str(stage) + '_blk'
+    pool_name_base = 'pool' + str(stage)
+
+    x = BatchNormalization(epsilon=eps, axis=concat_axis, name=conv_name_base+'_bn')(x)
+    x = Scale(axis=concat_axis, name=conv_name_base+'_scale')(x)
+    x = Activation('relu', name=relu_name_base)(x)
+    x = Conv2D(int(nb_filter * compression), (1, 1), name=conv_name_base, use_bias=False)(x)
+
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+
+    x = AveragePooling2D((2, 2), strides=(2, 2), name=pool_name_base)(x)
+
+    return x
+
+
+def dense_block(x, stage, nb_layers, nb_filter, growth_rate, dropout_rate=None, weight_decay=1e-4, grow_nb_filters=True):
+    ''' Build a dense_block where the output of each conv_block is fed to subsequent ones
+        # Arguments
+            x: input tensor
+            stage: index for dense block
+            nb_layers: the number of layers of conv_block to append to the model.
+            nb_filter: number of filters
+            growth_rate: growth rate
+            dropout_rate: dropout rate
+            weight_decay: weight decay factor
+            grow_nb_filters: flag to decide to allow number of filters to grow
+    '''
+
+    eps = 1.1e-5
+    concat_feat = x
+
+    for i in range(nb_layers):
+        branch = i+1
+        x = conv_block(concat_feat, stage, branch, growth_rate, dropout_rate, weight_decay)
+        concat_feat = concatenate([concat_feat, x],  axis=concat_axis, name='concat_'+str(stage)+'_'+str(branch))
+
+        if grow_nb_filters:
+            nb_filter += growth_rate
+
+    return concat_feat, nb_filter
+
+def get_categorical_accuracy_keras(y_true, y_pred):
+    return K.mean(K.equal(K.argmax(y_true, axis=1), K.argmax(y_pred, axis=1)))
+
+import numpy as np
+
+def get_categorical_accuracy(y_true, y_pred):
+    return (np.argmax(y_true, axis=1) == np.argmax(y_pred, axis=1)).mean()
+
+def bind_model(model):
+    def save(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+        model.save_weights(os.path.join(dir_name, 'model'))
+        print('model saved!')
+
+    def load(file_path):
+        model.load_weights(file_path)
+        print('model loaded!')
+
+    def infer(queries, db):
+
+        # Query 개수: 195
+        # Reference(DB) 개수: 1,127
+        # Total (query + reference): 1,322
+
+        queries, query_img, references, reference_img = preprocess(queries, db)
+
+        print('test data load queries {} query_img {} references {} reference_img {}'.
+              format(len(queries), len(query_img), len(references), len(reference_img)))
+
+        queries = np.asarray(queries)
+        query_img = np.asarray(query_img)
+        references = np.asarray(references)
+        reference_img = np.asarray(reference_img)
+
+        query_img = query_img.astype('float32')
+        query_img /= 255
+        reference_img = reference_img.astype('float32')
+        reference_img /= 255
+
+        get_feature_layer = K.function([model.layers[0].input] + [K.learning_phase()], [model.layers[-2].output])
+
+        print('inference start')
+
+        # inference
+        query_vecs = get_feature_layer([query_img, 0])[0]
+
+        # caching db output, db inference
+        db_output = './db_infer.pkl'
+        if os.path.exists(db_output):
+            with open(db_output, 'rb') as f:
+                reference_vecs = pickle.load(f)
+        else:
+            reference_vecs = get_feature_layer([reference_img, 0])[0]
+            with open(db_output, 'wb') as f:
+                pickle.dump(reference_vecs, f)
+
+        # l2 normalization
+        query_vecs = l2_normalize(query_vecs)
+        reference_vecs = l2_normalize(reference_vecs)
+
+        # Calculate cosine similarity
+        sim_matrix = np.dot(query_vecs, reference_vecs.T)
+
+        retrieval_results = {}
+
+        for (i, query) in enumerate(queries):
+            query = query.split('/')[-1].split('.')[0]
+            sim_list = zip(references, sim_matrix[i].tolist())
+            sorted_sim_list = sorted(sim_list, key=lambda x: x[1], reverse=True)
+
+            ranked_list = [k.split('/')[-1].split('.')[0] for (k, v) in sorted_sim_list]  # ranked list
+
+            retrieval_results[query] = ranked_list
+        print('done')
+
+        return list(zip(range(len(retrieval_results)), retrieval_results.items()))
+
+    # DONOTCHANGE: They are reserved for nsml
+    nsml.bind(save=save, load=load, infer=infer)
+
+
+def l2_normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+# data preprocess
+def preprocess(queries, db):
+    query_img = []
+    reference_img = []
+    img_size = (224, 224)
+
+    for img_path in queries:
+        img = cv2.imread(img_path, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, img_size)
+        query_img.append(img)
+
+    for img_path in db:
+        img = cv2.imread(img_path, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, img_size)
+        reference_img.append(img)
+
+    return queries, query_img, db, reference_img
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+
+    # hyperparameters
+    args.add_argument('--epochs', type=int, default=5)
+    args.add_argument('--batch_size', type=int, default=128)
+
+    # DONOTCHANGE: They are reserved for nsml
+    args.add_argument('--mode', type=str, default='train', help='submit일때 해당값이 test로 설정됩니다.')
+    args.add_argument('--iteration', type=str, default='0', help='fork 명령어를 입력할때의 체크포인트로 설정됩니다. 체크포인트 옵션을 안주면 마지막 wall time 의 model 을 가져옵니다.')
+    args.add_argument('--pause', type=int, default=0, help='model 을 load 할때 1로 설정됩니다.')
+    config = args.parse_args()
+
+    ## edit start
+
+    ## end
+    # training parameters
+    nb_epoch = config.epochs
+    batch_size = config.batch_size
+    num_classes = 1000
+    input_shape = (224, 224, 3)  # input image shape
+
+    """ Model """
+
+    model = DenseNet()
+    model.summary()
+
+    bind_model(model)
+
+    if config.pause:
+        nsml.paused(scope=locals())
+
+    bTrainmode = False
+    if config.mode == 'train':
+        bTrainmode = True
+
+        """ Initiate RMSprop optimizer """
+        opt = keras.optimizers.rmsprop(lr=0.00045, decay=1e-6)
+        model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=[get_categorical_accuracy_keras])
+        # model.compile(loss='categorical_crossentropy',
+        #               optimizer=opt,
+        #               metrics=['accuracy'])
+
+        """ Load data """
+        print('dataset path', DATASET_PATH)
+        output_path = ['./img_list.pkl', './label_list.pkl']
+        train_dataset_path = DATASET_PATH + '/train/train_data'
+        print('train_dataset_path', train_dataset_path)
+
+        if nsml.IS_ON_NSML:
+            # Caching file
+            nsml.cache(train_data_loader, data_path=train_dataset_path, img_size=input_shape[:2],
+                       output_path=output_path)
+        else:
+            # local에서 실험할경우 dataset의 local-path 를 입력해주세요.
+            train_data_loader(train_dataset_path, input_shape[:2], output_path=output_path)
+
+        with open(output_path[0], 'rb') as img_f:
+            img_list = pickle.load(img_f)
+        with open(output_path[1], 'rb') as label_f:
+            label_list = pickle.load(label_f)
+
+        x_train = np.asarray(img_list)
+        labels = np.asarray(label_list)
+        y_train = keras.utils.to_categorical(labels, num_classes=num_classes)
+        x_train = x_train.astype('float32')
+        x_train /= 255
+        print(len(labels), 'train samples')
+
+        """ Callback """
+        monitor = 'get_categorical_accuracy_keras'
+        reduce_lr = ReduceLROnPlateau(monitor=monitor, patience=3)
+
+        """ Training loop """
+        for epoch in range(nb_epoch):
+            res = model.fit(x_train, y_train,
+                            batch_size=batch_size,
+                            initial_epoch=epoch,
+                            epochs=epoch + 1,
+                            callbacks=[reduce_lr],
+                            verbose=1,
+                            shuffle=True)
+            print(res.history)
+            train_loss, train_acc = res.history['loss'][0], res.history['get_categorical_accuracy_keras'][0]
+            nsml.report(summary=True, epoch=epoch, epoch_total=nb_epoch, loss=train_loss, acc=train_acc)
+            nsml.save(epoch)
